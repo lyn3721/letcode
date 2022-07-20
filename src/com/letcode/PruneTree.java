5bcfe8d7d08b01d04f78966989831cbae5c6b5d0
liuyingnan <1755873797@qq.com>
2022-07-21 00:29:26 +0800

    [814]二叉树剪枝

---
+++ src/com/letcode/PruneTree.java
@@ -0,0 +1,39 @@
+package com.letcode;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+import javax.swing.tree.TreeNode;
+
+/**
+ * Definition for a binary tree node.
+ *
+ */
+class PruneTree {
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public TreeNode pruneTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        root.left = pruneTree(root.left);
+        root.right = pruneTree(root.right);
+        if (root.left == null && root.right == null && root.val == 0) {
+            return null;
+        }
+        return root;
+    }
+
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
