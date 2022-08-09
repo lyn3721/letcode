0c8850ac9819bdd0c84a9a392967a0d7d84819ef
liuyingnan <1755873797@qq.com>
2022-08-09 23:29:11 +0800

    1413. 逐步求和得到正数的最小值

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -7,3 +7,11 @@ I
 	src/a.txt,0/d/0d8b587aceb3b3b0f251c5b40310f8c772942dd6
 X
 (src/com/letcode/queue/CircularQueue.java,9/9/99d9a1b2bbc764c272ff3e2d3798e647324d47a6
+
+Osrc/com/letcode/str/Generate_a_string_with_characters_that_have_odd_counts.java,b/4/b411d9c5db38911d1bb444fd9d1781140bc06586
+W
+'src/com/letcode/queue/OrderlyQueue.java,5/8/5857e986731ba1cd2b501c4bf80792bf71f3e427
+h
+8src/com/letcode/tree/MaximumLevelSumOfOneBinaryTree.java,1/f/1f7e959943db720d1be54647739840f343172894
+S
+#.idea/sonarlint/issuestore/index.pb,9/f/9fe84ebb15faf917b7def6236dba604453cc61e0
---
+++ src/com/letcode/MINIMUM_VALUE_TO_GET_POSITIVE_STEP_BY_STEP_SUM.java
@@ -0,0 +1,48 @@
+package com.letcode;
+
+import java.util.Arrays;
+
+/**
+ * 1413. 逐步求和得到正数的最小值
+ *
+ * @author lyn
+ * @since 2022/8/9 23:14
+ */
+public class MINIMUM_VALUE_TO_GET_POSITIVE_STEP_BY_STEP_SUM {
+    /**
+     * 给你一个整数数组 nums。你可以选定任意的正数 startValue 作为初始值。
+     *
+     * 你需要从左到右遍历 nums数组，并将 startValue 依次累加上nums数组中的值。
+     *
+     * 请你在确保累加和始终大于等于 1 的前提下，选出一个最小的正数作为 startValue 。
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode.cn/problems/minimum-value-to-get-positive-step-by-step-sum
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+    public static int minStartValue(int[] nums) {
+//        int[] arr = new int[nums.length];
+//        int sum = 0;
+//
+//        for (int i = 0; i < nums.length; i++) {
+//            arr[i] = sum + nums[i];
+//            sum += nums[i];
+//        }
+//        int[] ints = Arrays.stream(arr).sorted().toArray();
+//        int i = ints[0];
+//        if (i > 0) {
+//            return 1;
+//        }
+//        return Math.abs(i) + 1;
+        int accSum = 0, accSumMin = 0;
+        for (int num : nums) {
+            accSum += num;
+            accSumMin = Math.min(accSumMin, accSum);
+        }
+        return -accSumMin + 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(minStartValue(new int[]{1,2}));
+    }
+}
