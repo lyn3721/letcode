b2b7103feaea47fc83dc1bcf174da347a5b9b179
liuyingnan <1755873797@qq.com>
2022-11-01 19:17:45 +0800

    [1662] 检查两个字符串数组是否相等

---
+++ src/com/letcode/str/Class_1662.java
@@ -0,0 +1,8 @@
+package com.letcode.str;/**
+ * https://leetcode.cn/problems/check-if-two-string-arrays-are-equivalent/
+ *                  
+ * @author lyn 
+ * @since 2022/11/1 18:59
+ */
+public class Class_1662 {
+}
